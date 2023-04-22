540b3bac6e9d376566550f53d813d1b5bacd858b
Nico <nico.nel@structureit.net>
2023-04-22 23:41:16 +0200

    Build search input

--- src/components/ColorModeSwitch.tsx
+++ src/components/ColorModeSwitch.tsx
@@ -6,8 +6,12 @@ const ColorModeSwitch = () => {
   return (
     <div>
       <HStack>
-        <Switch colorScheme="green" isChecked={colorMode === 'dark'} onChange={toggleColorMode} />
-        <Text>Dark Mode</Text>
+        <Switch
+          colorScheme="green"
+          isChecked={colorMode === "dark"}
+          onChange={toggleColorMode}
+        />
+        <Text whiteSpace="nowrap">Dark Mode</Text>
       </HStack>
     </div>
   );
