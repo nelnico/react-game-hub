b17ce8a871772aabc44b9356eef0d7cc692c2938
Nico <nico.nel@structureit.net>
2023-04-22 15:53:39 +0200

    Refactor: remove duplicated code

---
+++ src/components/GameCardContainer.tsx
@@ -0,0 +1,14 @@
+import { Box } from "@chakra-ui/react";
+
+interface Props {
+  children: React.ReactNode;
+}
+const GameCardContainer = ({ children }: Props) => {
+  return (
+    <Box width="300px" borderRadius={10} overflow="hidden">
+      {children}
+    </Box>
+  );
+};
+
+export default GameCardContainer;
