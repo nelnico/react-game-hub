72bcdf38a9137d603885ee336f73eaab824d5172
Nico <nico.nel@structureit.net>
2023-04-22 22:10:05 +0200

    Filter games by platform

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -16,6 +16,12 @@ export interface Platform {
 }
 
 
-const useGames = (selectedGenre: Genre | null) => useData<Game>("/games", { params: { genres: selectedGenre?.id } }, [selectedGenre?.id]);
+const useGames = (selectedGenre: Genre | null, selectedPlatform: Platform | null) => useData<Game>("/games", {
+    params: {
+        genres: selectedGenre?.id,
+        parent_platforms: selectedPlatform?.id
+    }
+},
+    [selectedGenre?.id, selectedPlatform?.id]);
 
 export default useGames;
